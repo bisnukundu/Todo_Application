3a2aa892ef51327ab667da7bd28606fcc3a8f306
bisnukundu <bisnkundu@gmail.com>
2020-01-17 15:56:27 +0600

    1/17/2020

--- javascript.js
+++ javascript.js
@@ -1,35 +1,40 @@
 let input_box = document.querySelector(".input_box");
 let add_btn = document.querySelector(".add_btn");
 let table = document.querySelector(".tb");
-let data = []
+let tbody = document.querySelector(".tb tbody");
+let data = [];
+let d = 0;
+function test(name, id) {
+	let new_row = table.insertRow(1);
+	new_row.setAttribute("id", id)
+	let name_cell = new_row.insertCell(0)
+	name_cell.innerHTML = name;
+	let action_cell = new_row.insertCell(1)
+	let del_btn = document.createElement("button")
+	del_btn.innerText = "Delete"
+	action_cell.appendChild(del_btn);
 
-
-for (let i = 0; i < data.length; i++) {
-	let row = table.insertRow(1)
-	let name = row.insertCell(0)
-	let action = row.insertCell(1)
-	name.innerHTML = data[i];
+	del_btn.addEventListener("click", (e) => {
+		data.splice(id, 1)
+		console.log(e.target.parentNode.parentNode.parentNode.removeChild(new_row))
+		console.log(data)
+	})
 }
 
-input_box.addEventListener("keyup", function(e) {
+add_btn.addEventListener("click", function() {
+	let value = input_box.value;
+	let final = test(value, 3)
+	data.push(value)
+	input_box.value = "";
+})
+
+
+
+input_box.addEventListener("keyup", (e) => {
 	if (e.keyCode == 13) {
-		let del_btn = document.createElement("button");
-
-
-	del_btn.addEventListener("click",function(e){
-
-		action.parentNode.parentNode.removeChild(row)
-
-	})
-		del_btn.innerText = "Delete"
-		let row = table.insertRow(1)
-		let name = row.insertCell(0)
-		let action = row.insertCell(1)
-		let d_show = name.innerHTML = input_box.value;
-		input_box.value = ""
-		data.push(d_show)
-		action.appendChild(del_btn);
-	} else {
-
+		let value = input_box.value;
+		test(value, d++)
+		data.push(value)
+		input_box.value = "";
 	}
 })
